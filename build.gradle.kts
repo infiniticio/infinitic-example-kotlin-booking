812e7f4c2af0457e9288f59331ec6f2704d96bd1
Gilles Barbier <gilles@zenaton.com>
2021-01-29 23:51:54 +0100

    use 0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,13 +13,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.+")
     implementation("org.slf4j:slf4j-simple:1.7.+")
     // https://github.com/apache/pulsar/issues/9045
     implementation("org.apache.avro:avro") { version { strictly("1.9.+") } }
 
-    implementation("io.infinitic:infinitic-pulsar:0.1.0-SNAPSHOT")
-    implementation("io.infinitic:infinitic-client:0.1.0-SNAPSHOT")
+    implementation("io.infinitic:infinitic-pulsar:0.1.0")
+    implementation("io.infinitic:infinitic-client:0.1.0")
 }
 
 application {
